28cf7f6260904501b52fc57662f21281197db00b
Haowei Lin <linhw@a230.cluster.helixon.com>
2025-05-10 15:02:27 +0800

    enable chunked forward in bfs

--- methods/bfs.py
+++ methods/bfs.py
@@ -100,43 +100,53 @@ class BFSGuidance(BaseGuidance):
         temp = self.get_temp(t, alpha_prod_ts, alpha_prod_t_prevs)
         i = t     # i is in index space
         t = ts[t]   # convert from int space to tensor space
-
         for recur_step in range(self.args.recur_steps):
+            chunked_x = x.chunk(2) if x.shape[0] > 8 else [x,]
+            x0_list = []
+            x_prev_list = []
+            x_list = []
+            for x in chunked_x:
+                # sample noise to estimate the \tilde p distribution
+                mc_eps = self.get_noise(std, x.shape, self.args.eps_bsz, **kwargs)
 
-            # sample noise to estimate the \tilde p distribution
-            mc_eps = self.get_noise(std, x.shape, self.args.eps_bsz, **kwargs)
+                # Compute guidance on x_t, and obtain Delta_t
+                if rho != 0.0:
+                    with torch.enable_grad():
+                        x_g = x.clone().detach().requires_grad_()
+                        x0 = self._predict_x0(x_g, unet(x_g, t), alpha_prod_t, **kwargs)
 
-            # Compute guidance on x_t, and obtain Delta_t
-            if rho != 0.0:
-                with torch.enable_grad():
-                    x_g = x.clone().detach().requires_grad_()
-                    x0 = self._predict_x0(x_g, unet(x_g, t), alpha_prod_t, **kwargs)
+                        logprobs = self.tilde_get_guidance(
+                            x0, mc_eps, return_logp=True, **kwargs)
+                        Delta_t = grad(logprobs.sum(), x_g)[0]
+                        Delta_t = rescale_grad(Delta_t, clip_scale=self.args.clip_scale, **kwargs)
+                        Delta_t = Delta_t * rho
+                        
+                else:
+                    Delta_t = torch.zeros_like(x)
+                    x0 = self._predict_x0(x, unet(x, t), alpha_prod_t, **kwargs)
 
-                    logprobs = self.tilde_get_guidance(
-                        x0, mc_eps, return_logp=True, **kwargs)
-                    Delta_t = grad(logprobs.sum(), x_g)[0]
-                    Delta_t = rescale_grad(Delta_t, clip_scale=self.args.clip_scale, **kwargs)
-                    Delta_t = Delta_t * rho
-                    
-            else:
-                Delta_t = torch.zeros_like(x)
-                x0 = self._predict_x0(x, unet(x, t), alpha_prod_t, **kwargs)
+                # Compute guidance on x_{0|t}
+                new_x0 = x0.clone().detach()
+                for _ in range(self.args.iter_steps):
+                    if mu != 0.0:
+                        new_x0 += mu * self.tilde_get_guidance(
+                            new_x0.detach().requires_grad_(), mc_eps, **kwargs)
+                Delta_0 = new_x0 - x0
+                
+                # predict x_{t-1} using S(zt, hat_epsilon, t), this is also DDIM sampling
+                alpha_t = alpha_prod_t / alpha_prod_t_prev
+                x_prev = self._predict_x_prev_from_zero(
+                    x, x0, alpha_prod_t, alpha_prod_t_prev, eta, t, **kwargs)
+                x_prev += Delta_t / alpha_t ** 0.5 + Delta_0 * alpha_prod_t_prev ** 0.5
 
-            # Compute guidance on x_{0|t}
-            new_x0 = x0.clone().detach()
-            for _ in range(self.args.iter_steps):
-                if mu != 0.0:
-                    new_x0 += mu * self.tilde_get_guidance(
-                        new_x0.detach().requires_grad_(), mc_eps, **kwargs)
-            Delta_0 = new_x0 - x0
-            
-            # predict x_{t-1} using S(zt, hat_epsilon, t), this is also DDIM sampling
-            alpha_t = alpha_prod_t / alpha_prod_t_prev
-            x_prev = self._predict_x_prev_from_zero(
-                x, x0, alpha_prod_t, alpha_prod_t_prev, eta, t, **kwargs)
-            x_prev += Delta_t / alpha_t ** 0.5 + Delta_0 * alpha_prod_t_prev ** 0.5
+                x = self._predict_xt(x_prev, alpha_prod_t, alpha_prod_t_prev, **kwargs).detach().requires_grad_(False)
 
-            x = self._predict_xt(x_prev, alpha_prod_t, alpha_prod_t_prev, **kwargs).detach().requires_grad_(False)
+                x0_list.append(x0)
+                x_prev_list.append(x_prev)
+                x_list.append(x)
+            x0 = torch.cat(x0_list, dim=0)
+            x_prev = torch.cat(x_prev_list, dim=0)
+            x = torch.cat(x_list, dim=0)
 
         if i in self.resampling_steps() and temp > 0:
             if bon_guider:
