0d1914e83375bf89294f3443c5a4ec29af50a60c
Haowei Lin <linhw@a229.cluster.helixon.com>
2025-05-11 10:45:10 +0800

    prepare rebase for 222

--- scripts/bfs/rebase_222.sh
+++ scripts/bfs/rebase_222.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=rebase_222_t1             # Job name
+#SBATCH --job-name=rebase12_222_t1             # Job name
 #SBATCH -o status/myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e status/myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1                    # Request 1 GPU
 #SBATCH --mem=20G                       # Memory
-#SBATCH --time=8:00:00                 # Max runtime
+#SBATCH --time=16:00:00                 # Max runtime
 #SBATCH --partition=gpu                 # Adjust to your cluster
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=504985967@qq.com
@@ -33,7 +33,7 @@ eta=1.0
 clip_x0=True
 seed=42
 logging_dir='logs'
-per_sample_batch_size=8
+per_sample_batch_size=12
 num_samples=256
 logging_resolution=512
 guidance_name='bfs-resample'
@@ -49,7 +49,7 @@ iter_steps=4
 
 start=25
 step_size=25
-temp=1.5
+temp=1.0
 
 # Run
 python main.py \
