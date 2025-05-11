f843de136dd5ee2f8fa157e3cd11ba98c6bd5635
Haowei Lin <linhw@a229.cluster.helixon.com>
2025-05-11 10:43:30 +0800

    prepare bfs for 222

--- scripts/bfs/prune_222.sh
+++ scripts/bfs/prune_222.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=prune_222_t0.5             # Job name
+#SBATCH --job-name=prune16_222_t1             # Job name
 #SBATCH -o status/myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e status/myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1                    # Request 1 GPU
 #SBATCH --mem=20G                       # Memory
-#SBATCH --time=6:00:00                 # Max runtime
+#SBATCH --time=16:00:00                 # Max runtime
 #SBATCH --partition=gpu                 # Adjust to your cluster
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=504985967@qq.com
@@ -14,7 +14,7 @@
 # module load python/3.8 cuda/11.7  (example)
 
 # Set CUDA device (optional, Slurm will often handle this)
-export CUDA_VISIBLE_DEVICES=7,
+# export CUDA_VISIBLE_DEVICES=7,
 
 # Parameters
 data_type=image
@@ -51,7 +51,7 @@ start=25
 step_size=25
 # temp=2.0
 # temp=1.0
-temp=1.5
+temp=1.0
 
 # Run
 python main.py \
